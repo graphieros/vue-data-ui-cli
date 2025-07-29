fd53566c813f63d220688f749ead198ff6b3f54e
graphieros <alec.lloyd.probert@gmail.com>
2025-07-29 10:50:18 +0200

    Bump vue-data-ui to 2.17.6

--- index.js
+++ index.js
@@ -476,7 +476,7 @@ const supportedComponents = {
 })()
 
 program
-  .version('1.12.5')
+  .version('1.12.6')
   .description('CLI to generate Vue Data UI component boilerplates')
   .action(async () => {
     const answers = await inquirer.prompt([
