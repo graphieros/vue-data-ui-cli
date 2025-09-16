4b9957e9ef6918ce412803f17846865f410d4d14
graphieros <alec.lloyd.probert@gmail.com>
2025-09-16 14:12:18 +0200

    3.1.8

--- index.js
+++ index.js
@@ -476,7 +476,7 @@ const supportedComponents = {
 })()
 
 program
-  .version('3.1.7')
+  .version('3.1.8')
   .description('CLI to generate Vue Data UI component boilerplates')
   .action(async () => {
     const answers = await inquirer.prompt([
