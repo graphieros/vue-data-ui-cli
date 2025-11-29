a63b6b1f3d46a3afe6a200ee72a6d3a908c009cb
graphieros <alec.lloyd.probert@gmail.com>
2025-11-29 11:22:34 +0100

    3.7.14

--- index.js
+++ index.js
@@ -484,7 +484,7 @@ const supportedComponents = {
 })()
 
 program
-  .version('3.7.13')
+  .version('3.7.14')
   .description('CLI to generate Vue Data UI component boilerplates')
   .action(async () => {
     const answers = await inquirer.prompt([
